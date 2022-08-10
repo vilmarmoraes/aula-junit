0261d85942bb333842e966319d38928399929ba4
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-08-10 14:10:04 -0300

    finished account tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -8,42 +8,61 @@ import tests.factory.AccountFactory;
 
 public class AccountTests {
 
-	
 	@Test
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
-		
+
 		double amount = 200.0;
-		double expectedValue = 196.0;		
-		//Account acc = new Account(1L, 0.0);
+		double expectedValue = 196.0;
+		// Account acc = new Account(1L, 0.0);
 		Account acc = AccountFactory.createEmptyAccount();
-		
-		acc.deposit(amount);		
+
+		acc.deposit(amount);
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
-	
 
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
-		
-		double expectedValue = 100.0;		
+
+		double expectedValue = 100.0;
 //		Account acc = new Account(1L, expectedValue);
 		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.0;
-		
-		acc.deposit(amount);		
+
+		acc.deposit(amount);
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 
 	@Test
 	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
-		
-		double expectedValue = 0.0;		
+
+		double expectedValue = 0.0;
 		double initialBalance = 800.0;
 		Account acc = AccountFactory.createAccount(initialBalance);
-		
+
 		double result = acc.fullWithdraw();
 		Assertions.assertTrue(expectedValue == acc.getBalance());
 		Assertions.assertTrue(result == initialBalance);
 	}
-	
+
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+
+		Account acc = AccountFactory.createAccount(800.0);
+
+		acc.withdraw(500.0);
+
+		Assertions.assertEquals(300.0, acc.getBalance());
+
+	}
+
+	@Test
+	public void withdrawShouldThrowExceptionWhenInSufficientBalance() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(800.0);
+
+			acc.withdraw(801.0);
+		});
+
+	}
 }
