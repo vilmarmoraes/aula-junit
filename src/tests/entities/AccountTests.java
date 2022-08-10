dfd685b16dad4e1bddb0b9a49e7c903d0d5f4a06
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-08-10 13:53:49 -0300

    added test three

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,16 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
 	
 	@Test
-	public void depositShoudIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;		
-		Account acc = new Account(1L, 0.0);
+		//Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
 		
 		acc.deposit(amount);		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
@@ -21,15 +23,27 @@ public class AccountTests {
 	
 
 	@Test
-	public void depositShoudDoNothingWhenNegativeAmount() {
+	public void depositShouldDoNothingWhenNegativeAmount() {
 		
 		double expectedValue = 100.0;		
-		Account acc = new Account(1L, expectedValue);
+//		Account acc = new Account(1L, expectedValue);
+		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.0;
 		
 		acc.deposit(amount);		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
-	
+
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;		
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
 	
 }
